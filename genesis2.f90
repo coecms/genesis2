36d3dd8fca801b66fa28d54d8021d8a75ad99fdf
Holger Wolff <holger.wolff@monash.edu>
2014-09-10 18:35:42 +1000

    reading INDATA from template

--- genesis2.f90
+++ genesis2.f90
@@ -1,5 +1,16 @@
 program genesis2
+  
+  use mod_indata
 
   implicit none
+
+  call init_indata(1, 1)
+  call default_indata(1, 1)
+
+  open(50, file='template.scm')
+  read(50,INDATA)
+
+  write(*, INDATA)
+  close(50)
 
 end program genesis2
